7b42a41f7b2401ac3c9b7e9998b1963df7608ac8
sidhantrajput1 <sidhant808310@gmail.com>
2025-01-27 14:16:04 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="">
+      <h3 className="font-bold underline text-xl text-red-500">halo world</h3>
+    </div>
+  )
+}
+
+export default App;
