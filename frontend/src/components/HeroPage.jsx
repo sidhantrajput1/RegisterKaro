856f488a5571953e6abfdc2ba98e9327105dd417
sidhantrajput1 <sidhant808310@gmail.com>
2025-01-28 12:27:18 +0530

    HeroPage

--- frontend/src/components/HeroPage.jsx
+++ frontend/src/components/HeroPage.jsx
@@ -1,9 +1,10 @@
-
+// import bgSetImage from './../assets/img/bg.png'
 
 export const HeroPage = () => {
     return (
-        <div className="">
-
+        <div className="bg-[url('/bgSet.png')] bg-cover bg-center min-h-screen">
+            <h3>hello</h3>
         </div>
-    )
+    );
 }
+
