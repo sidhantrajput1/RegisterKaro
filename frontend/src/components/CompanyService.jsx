593e87bd5d4db9bfd7d7d720b7e5bfcd753cfea3
sidhantrajput1 <sidhant808310@gmail.com>
2025-02-03 20:49:11 +0530

    Conapnntta

--- frontend/src/components/CompanyService.jsx
+++ frontend/src/components/CompanyService.jsx
@@ -1,7 +1,7 @@
 import Oracle from './../assets/CompanyImg/Oracle_logo.png';
-import Segment from './../assets/CompanyImg/segmentLogo.png';
-import Monday from './../assets/CompanyImg/MondayLogo.png';
-import Sumsung from './../assets/CompanyImg/SumsungLogo.png';
+// import Segment from './../assets/CompanyImg/segmentLogo.png';
+// import Monday from './../assets/CompanyImg/MondayLogo.png';
+// import Sumsung from './../assets/CompanyImg/SumsungLogo.png';
 
 function CompanyService() {
     return (
@@ -11,10 +11,10 @@ function CompanyService() {
             </h3>
             <div className="company grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-8 items-center justify-items-center px-5">
                 <img className="w-[120px] md:w-[150px]" src={Oracle} alt="Oracle Logo" />
-                <img className="w-[120px] md:w-[150px]" src={Segment} alt="Segment Logo" />
-                <img className="w-[120px] md:w-[150px]" src={Sumsung} alt="Samsung Logo" />
-                <img className="w-[120px] md:w-[150px]" src={Monday} alt="Monday Logo" />
-                <img className="w-[120px] md:w-[150px]" src={Segment} alt="Segment Logo" />
+                <img className="w-[120px] md:w-[150px]" src={Oracle} alt="Segment Logo" />
+                <img className="w-[120px] md:w-[150px]" src={Oracle} alt="Samsung Logo" />
+                <img className="w-[120px] md:w-[150px]" src={Oracle} alt="Monday Logo" />
+                <img className="w-[120px] md:w-[150px]" src={Oracle} alt="Segment Logo" />
             </div>
         </div>
     );
